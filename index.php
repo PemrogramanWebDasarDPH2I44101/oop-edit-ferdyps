552fdd4d80b5cebafa2ad60d230201c9e77fdfa5
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-10-29 18:33:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Input Data</title>
 </head>
 <body>
-    <form action="class.php" method="post">
-        Masukan angka 1<input type="text" name="input1"><br>
-        Masukan angka 2<input type="text" name="input2"><br>
-        Pilih Operasi
-        <select name="operasi">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
-        <br>
-        <input type="submit" value="Kirim">
+    <form action="class.php?tambah_data=mahasiswa" method="post">
+        <table>
+            <tr>
+                <td>NAMA</td>
+                <td><input type="text" name="nama"></td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td><input type="text" name="nim" pattern=\d*></td>
+            </tr>
+            <tr>
+                <td>TANGGAL LAHIR</td>
+                <td><input type="date" name="tanggal"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Submit"></td>
+            </tr>
+            <tr>
+                <td><a href="data.php">Lihat Data</a></td>
+            </tr>
+        </table>
     </form>
 </body>
 </html>
